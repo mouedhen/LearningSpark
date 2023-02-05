885bd29902d06dadd19c005e0c9123e0cbb63be6
Mohamed Chams Eddin Mouedhen <mohamed.mouedhen@mass-analytics.com>
2023-02-05 15:38:50 +0100

    Getting Started

---
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -0,0 +1,29 @@
+package com.virtualpairprogrammers;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(35.5);
+        inputData.add(12.499_43);
+        inputData.add(90.32);
+        inputData.add(20.32);
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("statingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+
+        sc.close();
+    }
+}
